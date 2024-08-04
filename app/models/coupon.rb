3592c95bca671bbae6889586be0f2c9b83976920
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-08-04 15:41:42 -0600

    Add validations to Coupon model, update Coupon model test (passing), run migration to add Coupon FK to Invoices

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,9 @@
 class Coupon < ApplicationRecord
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :type, presence: true
+  
   belongs_to :merchant
   has_many :invoices
 end
